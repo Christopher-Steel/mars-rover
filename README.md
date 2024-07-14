c0c66bc81dce99b75fad1572b8b024b5b2b8f907
Christopher-Steel <christopher.g.steel@gmail.com>
2024-07-14 16:02:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mars Rover
+
+Tool to calculate final positions of Mars rovers given a trajectory string and an initial position within a specified area grid.
+
+- **Github repository**: <https://github.com/Christopher-Steel/mars-rover/>
---
+++ run_mars_rover.sh
@@ -0,0 +1,9 @@
+# see https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425 if
+# this set command is unfamiliar to you for a great explanation
+#
+# They may not all be relevant in the current form of this script but it is
+# good practice to always include them so that they are there if the script
+# grows and needs them
+set -euo pipefail
+
+PYTHONPATH=$(pwd) poetry run python -m crawler "$@"
